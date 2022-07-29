e37cdab3a97e6ee4d9755796d0a3460f4935b2aa
Prajwel2008 <110116625+Prajwel2008@users.noreply.github.com>
2022-07-29 19:28:42 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weights=[53,36,48,27]
+sum= weights [0]+weights[1]+weights[2]+weights[3]
+console.log(sum)
+avg = sum/weights.length
+console.log(avg)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
